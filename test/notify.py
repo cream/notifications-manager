798fa80d51394b76d78c0915d59a99c4fc327e22
Friedrich Weber <fred@reichbier.de>
2010-06-09 01:33:13 +0200

    Never expire.

--- test/notify.py
+++ test/notify.py
@@ -4,7 +4,7 @@ import gtk
 if pynotify.init("My Application Name"):
     n = pynotify.Notification("Title", "message")
     n.set_urgency(pynotify.URGENCY_CRITICAL)
-    n.set_timeout(1000)
+    n.set_timeout(pynotify.EXPIRES_NEVER)
 
     def _callback(notification, action, data):
         print '%r %r %r' % (notification, action, data)
